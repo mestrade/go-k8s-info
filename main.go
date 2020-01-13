fea290575be86892106e1a565fb73cf282ca8ee9
mestrade <matthieu.estrade@gmail.com>
2020-01-13 17:11:42 +0100

    Test change

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello, jacques!")
 
 	vars := os.Environ()
 
