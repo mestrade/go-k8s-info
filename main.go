e9055e9016d29e99cf4e5839f23305709c2df0dd
mestrade <matthieu.estrade@gmail.com>
2020-01-16 13:00:29 +0100

    Add an external dependency

--- main.go
+++ main.go
@@ -2,13 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{} // use default options
+
 func main() {
+	http.HandleFunc("/echo", echo)
 	http.HandleFunc("/", HelloServer)
 	http.ListenAndServe(":8080", nil)
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
